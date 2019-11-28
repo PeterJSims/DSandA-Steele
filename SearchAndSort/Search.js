5a80438d42b989f35ab51318cda2d3ab48be4e31
Peter Sims <ragtimesims@gmail.com>
2019-11-27 22:13:05 -0500

    linear and binary search

--- SearchAndSort/Search.js
+++ SearchAndSort/Search.js
@@ -1,18 +1,22 @@
-//Linear Search -- Best for unsorted data
+//Liner search
+
 const linearSearch = (arr, val) => {
 	for (let i = 0; i < arr.length; i++) {
-		if (arr[i] === val) return i;
+		if (arr[i] === val) {
+			return i;
+		}
 	}
 	return -1;
 };
 
-//Binary Search
+//Binary search
+
 const binarySearch = (arr, val) => {
 	let left = 0;
 	let right = arr.length - 1;
 	let middle = Math.floor((left + right) / 2);
 
-	while (arr[middle] !== val && left <= right) {
+	while (left <= right && arr[middle] !== val) {
 		if (val < arr[middle]) {
 			right = middle - 1;
 		} else {
@@ -21,18 +25,5 @@ const binarySearch = (arr, val) => {
 		middle = Math.floor((left + right) / 2);
 	}
 
-	return arr[middle] === elem ? middle : -1;
+	return arr[middle] === val ? middle : -1;
 };
-
-const naiveStringSearch = (long, short) => {
-	let count = 0;
-	for (let i = 0; i < long.length; i++) {
-		for (let j = 0; j < short.length; j++) {
-			if (short[j] !== long[i + j]) break;
-			if (j === short.length - 1) count++;
-		}
-	}
-	return count;
-};
-
-console.log(naiveStringSearch('lorie loled', 'lol'));
